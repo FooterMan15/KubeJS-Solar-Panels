6717a9f417bd4f5294242da8d0a4aaa8763a16ed
FooterMan13 | FM Studios <71946346+FooterMan15@users.noreply.github.com>
2023-08-16 07:29:30 -0400

    Initial commit

---
+++ fabric/src/main/java/dev/kjssp/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package dev.kjssp.fabric;
+
+import dev.kjssp.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
