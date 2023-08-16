47f52b13d160a67bf28eb6e19396960c6484dd39
FooterMan13 | FM Studios <71946346+FooterMan15@users.noreply.github.com>
2023-08-16 07:56:25 -0400

    *

--- forge/src/main/java/dev/kjssp/forge/KubeJSSolarPanelsForge.java
+++ forge/src/main/java/dev/kjssp/forge/KubeJSSolarPanelsForge.java
@@ -6,8 +6,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ExampleMod.MOD_ID)
-public class ExampleModForge {
-    public ExampleModForge() {
+public class KubeJSSolarPanelsForge {
+    public KubeJSSolarPanelsForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ExampleMod.init();
